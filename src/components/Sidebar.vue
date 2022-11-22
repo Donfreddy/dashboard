d38ba0b347ec475fc66d7be642a54e3ad635ceaa
Donfreddy <freddytamwo@gmail.com>
2022-11-22 06:23:52 +0100

    last commit

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,13 +4,13 @@
     <div
       :class="isOpen ? 'block' : 'hidden'"
       @click="isOpen = false"
-      class="fixed inset-0 z-20 transition-opacity bg-black opacity-50  lg:hidden"
+      class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"
     ></div>
     <!-- End Backdrop -->
 
     <div
       :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
-      class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-100  lg:translate-x-0 lg:static lg:inset-0"
+      class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-100 lg:translate-x-0 lg:static lg:inset-0"
     >
       <div class="flex items-center justify-center mt-20">
         <div class="w-2/3">
--- src/pages/AddProduct.vue
+++ src/pages/AddProduct.vue
@@ -1,5 +1,112 @@
 <template>
-  <div class="add-product">
-    <h1>This is  Add Products pages</h1>
-  </div>
+  <div>
+    <!-- 1 -->
+     <div class="container px-6 py-16 mx-auto">
+            <div class="items-center lg:flex">
+                <div class="w-full lg:w-1/2">
+                    <div class="lg:max-w-lg">
+                        <h1 class="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">Subscribe To The <span class="text-indigo-500">Newsletter</span></h1>
+
+                        <p class="mt-4 text-gray-600 dark:text-gray-400">be the first to knows when our <span class="font-medium text-indigo-500">Brand</span> is live</p>
+
+                        <div class="flex flex-col mt-8 space-y-3 lg:space-y-0 lg:flex-row">
+                            <input id="email" type="text" class="px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" placeholder="Email Address">
+                            
+                            <button class="w-full px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-lg lg:w-auto lg:mx-4 hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400">
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+                    <img class="w-full h-full max-w-md"  src="~@/assets/images/cartoon.jpg"  alt="#">
+                </div>
+            </div>
+        </div>
+
+    <!-- 2 -->
+    <div class="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row">
+      <div
+        class="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0"
+      >
+        <h1 class="mb-4 text-3xl font-medium text-white title-font sm:text-4xl">
+         Hi, I'am Don Freddy
+        </h1>
+        <p class="mb-8 leading-relaxed">
+          Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed
+          tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken
+          authentic tumeric truffaut hexagon try-hard chambray.
+        </p>
+        <div class="flex justify-center">
+          <button
+            class="inline-flex px-6 py-2 text-lg text-white bg-blue-500 border-0 rounded focus:outline-none hover:bg-blue-600"
+          >
+            Button
+          </button>
+          <button
+            class="inline-flex px-6 py-2 ml-4 text-lg text-gray-400 bg-gray-800 border-0 rounded focus:outline-none hover:bg-gray-700 hover:text-white"
+          >
+            Button
+          </button>
+        </div>
+      </div>
+      <div class="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
+        <img
+          class="object-cover object-center rounded-full"
+          alt="hero"
+         src="~@/assets/images/cartoon.jpg" 
+        />
+      </div>
+    </div>
+
+    <!-- 3 -->
+     <div class="container px-6 py-16 mx-auto">
+            <div class="items-center lg:flex">
+                <div class="w-full lg:w-1/2">
+                    <div class="lg:max-w-lg">
+                        <h1 class="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl">Best Place To Choose Your Clothes</h1>
+                        <p class="mt-2 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                        <button class="w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-indigo-600 rounded-md lg:w-auto hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">Shop Now</button>
+                    </div>
+                </div>
+        
+                <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+                    <img class="w-full h-full lg:max-w-2xl" src="~@/assets/images/cartoon.jpg" alt="Catalogue-pana.svg">
+                </div>
+            </div>
+        </div>
+
+        <!-- Me -->
+         <div class="container flex flex-col px-5 py-24 mx-auto md:flex-row items-cente">
+      <div
+        class="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0"
+      >
+        <div class="lg:max-w-lg">
+          <h1 class="text-2xl font-semibold lg:text-3xl">Hi, I'am Freddy</h1>
+          <p class="mt-2 text-xs uppercase opacity-50 text-acColor">Full-stack Developer</p>
+
+          <p class="mt-4 leading-relaxed">
+            High level experience in web design and development knowledge, producing quality work.
+          </p>
+          <div class="flex justify-center">
+            <button
+              class="inline-flex px-6 py-2 text-lg text-white bg-blue-500 border-0 rounded focus:outline-none hover:bg-blue-600"
+            >
+              Button
+            </button>
+            <button
+              class="inline-flex px-6 py-2 ml-4 text-lg text-gray-700 bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+        <img src="~@/assets/images/cartoon.jpg" />
+      </div>
+    </div>
+    </div>
 </template>
